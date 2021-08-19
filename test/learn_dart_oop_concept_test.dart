bf069e4e22a13e7b2f4d2763b04b8dbdbff71de4
nando <muhammad.rezki.ananda@gmail.com>
2021-08-19 09:37:33 +0700

    INITIAL COMMIT

---
+++ test/learn_dart_oop_concept_test.dart
@@ -0,0 +1,8 @@
+import 'package:learn_dart_oop_concept/learn_dart_oop_concept.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
