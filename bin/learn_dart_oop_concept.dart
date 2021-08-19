cd42b9a739944efa37c4c2c9cd1ae8409e8c65ee
nando <muhammad.rezki.ananda@gmail.com>
2021-08-19 09:54:54 +0700

    First app

--- bin/learn_dart_oop_concept.dart
+++
@@ -1,5 +0,0 @@
-import 'package:learn_dart_oop_concept/learn_dart_oop_concept.dart' as learn_dart_oop_concept;
-
-void main(List<String> arguments) {
-  print('Hello world: ${learn_dart_oop_concept.calculate()}!');
-}
--- test/learn_dart_oop_concept_test.dart
+++
@@ -1,8 +0,0 @@
-import 'package:learn_dart_oop_concept/learn_dart_oop_concept.dart';
-import 'package:test/test.dart';
-
-void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
-  });
-}
