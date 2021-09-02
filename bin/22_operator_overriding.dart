b92cb0f07937fface7ee8e3d567af4905ecc74ca
nando <muhammad.rezki.ananda@gmail.com>
2021-09-02 19:47:48 +0700

    Operator overriding

---
+++ bin/22_operator_overriding.dart
@@ -0,0 +1,45 @@
+main(List<String> args) {
+  Hero a = Hero(20);
+  Hero b = Hero(20);
+
+  print((a + b).power);
+  print(a == b);
+  print(a > b);
+}
+
+class Hero {
+  final int power;
+
+  Hero(this.power);
+
+  Hero operator +(var other) {
+    if (other is Hero) {
+      return Hero(power + other.power);
+    } else if (other is int) {
+      return Hero(power + other);
+    } else {
+      return this;
+    }
+  }
+
+  @override
+  bool operator ==(var other) {
+    if (other is Hero) {
+      if (other.power == power) {
+        return true;
+      }
+    }
+
+    return false;
+  }
+
+  bool operator >(var other) {
+    if (other is Hero) {
+      if (power > other.power) {
+        return true;
+      }
+    }
+
+    return false;
+  }
+}
